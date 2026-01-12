d7f7164547ef6fd9bf4f6a0e07eea39cd81a2a86
kumamoto246x1151 <pe9346763@kisoab.ggl.kumamoto-u.ac.jp>
2026-01-13 00:02:33 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.write("最初の第一歩")
-st.write("ゲームを始める")
+st.write("NEW GAME")
